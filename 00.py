3d64af5922242f85323191aee1a0592fcd7fd1ce
vandana2238 <vandana.c38114@gmail.com>
2024-08-24 01:09:27 +0530

    completed basic python for ML

---
+++ 00.py
@@ -0,0 +1,47 @@
+#Q0
+
+# create a variable called color with an appropriate value on the line below
+# (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
+# color = 'blue'
+
+# # Check your answer
+# q0.check()
+
+# #Q1
+
+# pi = 3.14159 # approximate
+# diameter = 3
+
+# # Create a variable called 'radius' equal to half the diameter
+# radius = diameter / 2
+
+# # Create a variable called 'area', using the formula for the area of a circle: pi times the radius square
+# area = pi * (radius ** 2)
+
+# # Check your answer
+# q1.check()
+
+# #Q2
+
+# a = [1, 2, 3]
+# b = [3, 2, 1]
+# q2.store_original_ids()
+
+# temp = a
+# a = b
+# b = temp
+
+# # Check your answer
+# q2.check()
+
+# #Q3
+
+# # Variables representing the number of candies collected by alice, bob, and carol
+# alice_candies = 121
+# bob_candies = 77
+# carol_candies = 109
+
+# to_smash = (alice_candies + bob_candies + carol_candies) % 3
+
+# # Check your answer
+# q4.check()
